b054994eaf14085bede6b2650d96c4faf93e4fcb
rowaidalmaz <52274424+rowaidalmaz@users.noreply.github.com>
2023-08-21 11:06:44 +0300

    Add files via upload

---
+++ src/Troubleshooting.js
@@ -0,0 +1,31 @@
+// import Box from '@awsui/components-react/box';
+// import Button from '@awsui/components-react/button';
+// import ColumnLayout from '@awsui/components-react/column-layout';
+// import Container from '@awsui/components-react/container';
+// import Link from '@awsui/components-react/link';
+// import FormField from '@awsui/components-react/form-field';
+// import Grid from '@awsui/components-react/grid';
+// import Header from '@awsui/components-react/header';
+// import SpaceBetween from '@awsui/components-react/space-between';
+import './App.css';
+import React from 'react';
+
+
+
+function troubleshooting() {
+
+   return (
+      <div className="app">
+
+      <div>
+      <div class = "image" alt="img">
+      <img src="/background.png" alt="studio" width="1500" height="1300"></img>
+      </div>
+     </div>
+    </div>
+      );
+  
+
+}
+
+export default troubleshooting;
